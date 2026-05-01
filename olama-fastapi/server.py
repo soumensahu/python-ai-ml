a35e633779d759e1822482bdaad16aed29060156
soumensahu2011@gmail.com <soumensahu2011@gmail.com>
2026-05-01 07:12:18 +0530

    hugging face and olama handson

---
+++ olama-fastapi/server.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI,Body
+from ollama import Client
+
+
+app=FastAPI()
+
+client=Client(
+    host="http://localhost:11434"
+)
+
+@app.get("/")
+def read_root():
+    return "Hello world"
+
+@app.post("/chat")
+def chat(
+        message:str = Body(...,description="Message")
+):
+    response=client.chat(
+        model="gemma:2b",
+        messages=[
+            {"role":"user","content":message}
+        ]
+    )
+    return {"response":response.message.content}
